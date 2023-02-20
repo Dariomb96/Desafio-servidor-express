bc9c75209ed06c6168fd9a9d43876ec4c71d54e6
Dariomb96 <betancourtdariom@gmail.com>
2023-02-20 02:24:07 -0300

    cambiado sistema de persistencia a mongodb y agregado metodo GET para productos que cumpla con las consignas, falta cart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+require('dotenv').config();
 const app = express();
 const port = 8080;
 const httpServer = app.listen(port, () => {
     console.log(`listen on localhost:${port}`);
 });
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://${process.env.USER_MONGO}:${process.env.PASS_MONGO}@cluster0.5xfau6w.mongodb.net/${process.env.DB_MONGO}?retryWrites=true&w=majority`, (err) => {
+    if (err) {
+        console.log('cannot connect to database: ' + err);
+    } else { console.log('connected to database')}});
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set ('views', __dirname + '/views');
-const {cartRouter} = require('./routes/cartRouter');
-const {productRouter} = require('./routes/productRouter');
-const {viewsRouter} = require('./routes/viewsRouter');
-const socketServer = new Server(httpServer);
+    app.engine('handlebars', engine());
+    app.set('view engine', 'handlebars');
+    app.set('views', __dirname + '/views');
+    //const { cartRouter } = require('./routes/cartRouter');
+    //const { productRouter } = require('./routes/productRouter');
+    const { viewsRouter } = require('./routes/viewsRouter');
+    const socketServer = new Server(httpServer);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'/public'))
-app.use ('/api/products', productRouter);
-app.use ('/api/cart', cartRouter);
-app.use ('/', viewsRouter);
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(__dirname + '/public'))
+    //app.use('/api/products', productRouter);
+    //app.use('/api/cart', cartRouter);
+    app.use('/', viewsRouter);
 
-socketServer.on('connection', socket=>{
-    console.log('new client connected');
-    socket.on('disconnect', ()=> {
-        console.log('user disconnected');
+    socketServer.on('connection', socket => {
+        console.log('new client connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
     });
-});
--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,15 +1,71 @@
 const express = require('express');
 const viewsRouter = express.Router();
-const ProductManager = require('../ProductManager.js');
-let manager = new ProductManager('./products.json');
-const productos = manager.getProducts();
+const { productsModel } = require('../models/products');
 
-viewsRouter.get('', (req, res) => {
-    res.render('home', {productos});
+viewsRouter.get('', async (req, res) => {
+    try {
+        let products = await productsModel.find();
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 });
 
-viewsRouter.get('/realtimeproducts', (req, res) => {
-    res.render('realTimeProducts', {productos});
+viewsRouter.get('/products', async (req, res) => {
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    let sort = req.query.sort || null;
+    let query = {};
+
+    if (req.query.category) {
+        query.category = req.query.category;
+    }
+    if (req.query.status) {
+        query.status = req.query.status;
+    }
+
+    let options = {
+        page: page,
+        limit: limit,
+        lean: true,
+        sort: sort ? { price: sort === 'asc' ? 1 : -1 } : null,
+    };
+
+    try {
+        let result = await productsModel.paginate(query, options);
+        result.prevLink = result.hasPrevPage
+            ? `http://localhost:8080/products?page=${result.prevPage}`
+            : '';
+        result.nextLink = result.hasNextPage
+            ? `http://localhost:8080/products?page=${result.nextPage}`
+            : '';
+        result.isValid = !(page <= 0 || page > result.totalPages);
+        result.sortAsc = sort === null || sort === 'asc';
+        result.sortLink = sort ? `&sort=${sort}` : '';
+        res.render('products', result);
+    } catch (err) {
+        console.log(err);
+        res.render('error');
+    }
+});
+
+
+viewsRouter.post('', async (req, res) => {
+
+    try {
+        let { title, artist, price, thumbnail, code, stock, category, status } = req.body;
+        if (!title || !artist || !price || !thumbnail || !code || !stock || !category || !status) {
+            res.status(400).json({ error: 'Missing data' });
+            return;
+        } else {
+            let product = { title, artist, price, thumbnail, code, stock, category, status };
+            let result = await productsModel.create(product);
+            res.status(200).json({ result: "success", payload: result });
+        }
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 });
 
 module.exports = {
