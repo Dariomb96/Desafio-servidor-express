2a552febe6cf43e2f58b1ed5b2ff9a15af7bee00
Dariomb96 <betancourtdariom@gmail.com>
2023-06-21 23:07:54 -0300

    tuve que rearmar la logica del sendEmail xd

---
+++ src/public/js/ticket.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+const nameInput = document.getElementById('name');
+const totalInput = document.querySelector('input[name="total"]');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const total = parseFloat(totalInput.value);
+
+    fetch('/ruta-del-controlador/sendEmail', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            total: total
+        })
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
